43e077446fd9be4c4ae462bfa7b764bb7e2ba47b
silentlunar <tateross5@gmail.com>
2025-02-13 21:02:58 +0700

    feat: add Coordinates class

---
+++ src/main/java/com/silentlunar/Coordinates.java
@@ -0,0 +1,11 @@
+package src.main.java.com.silentlunar;
+
+public class Coordinates {
+    public final int x;
+    public final int y;
+
+    public Coordinates(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+}
--- src/main/java/com/silentlunar/MapWorld.java
+++ src/main/java/com/silentlunar/MapWorld.java
@@ -1,4 +1,16 @@
 package src.main.java.com.silentlunar;
+import src.main.java.com.silentlunar.entity.Entity;
+
+import java.util.HashMap;
 
 public class MapWorld {
+    public final int height; // высота
+    public final int width; // ширина
+
+    private HashMap<Coordinates, Entity> map = new HashMap<>();
+
+    public MapWorld(int height, int width) {
+        this.height = height;
+        this.width = width;
+    }
 }
