bbe2e3dcbfc02fc5cda76139237e8ce3a3d3bdc4
silentlunar <tateross5@gmail.com>
2025-02-27 02:00:57 +0700

    feat: added game simulation methods

--- src/main/java/com/silentlunar/Main.java
+++ src/main/java/com/silentlunar/Main.java
@@ -1,4 +1,45 @@
 package src.main.java.com.silentlunar;
 
+import java.util.Scanner;
+
 public class Main {
+    public static void main(String[] args) {
+        Simulation simulation = new Simulation();
+        printStartGame();
+        simulation.initMap();
+        Scanner scannerMenu;
+        boolean isSimulationGame = true;
+        while (isSimulationGame) {
+            scannerMenu = printMenuGame();
+            switch (scannerMenu.next()) {
+                case "1":
+                    simulation.nextTurn();
+                    break;
+                case "2":
+                    try {
+                        simulation.startSimulation();
+                    } catch (InterruptedException e) {
+                        throw new RuntimeException(e);
+                    }
+                    break;
+                case "3":
+                    simulation.pauseSimulation();
+                    isSimulationGame = false;
+                    break;
+            }
+        }
+    }
+
+    public static Scanner printMenuGame() {
+        System.out.println("~~~~~~~~~~~~~~~~~Меню~~~~~~~~~~~~~~~~~");
+        System.out.println("1~~~~~~~Симуляция~одного~хода~~~~~~~~~");
+        System.out.println("2~~~Бесконечная~симуляция~аквариума~~~");
+        System.out.println("3~~~~~~~~~~~~Закончить~игру~~~~~~~~~~~");
+        System.out.println("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
+        return new Scanner(System.in);
+    }
+    public static void printStartGame(){
+        System.out.println("~~~~~~~~~~~~~Игра~Симуляция~~~~~~~~~~~");
+        System.out.println("~~~~~~Сгенерированное~гровое~поле~~~~~");
+    }
 }
--- src/main/java/com/silentlunar/Simulation.java
+++ src/main/java/com/silentlunar/Simulation.java
@@ -1,10 +1,39 @@
 package src.main.java.com.silentlunar;
-import src.main.java.com.silentlunar.action.InitAction;
-import src.main.java.com.silentlunar.action.TurnAction;
+
+import src.main.java.com.silentlunar.actions.InitAction;
+import src.main.java.com.silentlunar.actions.TurnAction;
+import src.main.java.com.silentlunar.entities.Herbivore;
+import src.main.java.com.silentlunar.entities.Predator;
+
+import java.util.List;
+import java.util.concurrent.TimeUnit;
 
 public class Simulation {
-    private final InitAction initAction = new InitAction();
-    private final TurnAction turnAction = new TurnAction();
+
     private final Renderer renderer = new Renderer();
-    private MapWorld map;
+    private final MapWorld map = new MapWorld(15, 15);
+    private final InitAction initAction = new InitAction(map);
+    private final TurnAction turnAction = new TurnAction(map);
+    private boolean isGameOver = false;
+
+    public void initMap(){
+        initAction.initMap();
+        renderer.render(map);
+    }
+
+    public void startSimulation() throws InterruptedException {
+        while (!isGameOver) {
+            TimeUnit.SECONDS.sleep(1);
+            nextTurn();
+        }
+    }
+    public void nextTurn() {
+        turnAction.turnAction();
+        renderer.render(map);
+    }
+    public void pauseSimulation(){
+        isGameOver = true;
+        System.out.println("~~~~~~~~~~~~~~~Game~over~~~~~~~~~~~~~~");
+    }
+
 }
